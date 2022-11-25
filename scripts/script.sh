d52745a965b0ec027abd95c045643c22abc95693
Alejandro Godoy <alegodoy2013@gmail.com>
2022-11-25 20:52:55 -0300

    se creo el archivo sync.txt

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+
+#DIA=`date +"%Y/%m/%d"`
+#HORA=`date +"%H:%M"`
+
+#COMPRA=$(curl 127.0.0.1:5000/usd | ../jq .compra)
+#VENTA=$(curl 127.0.0.1:5000/usd | ../jq .venta)
+
+#echo "$DIA $HORA, $COMPRA, $VENTA" >> ../historico/historico.txt
+
+
+
+CAMBIO=$(stat -c %y sync.txt)
+
+while true
+do
+
+NuevoCambio=$(stat -c %y sync.txt)
+
+if [[ $CAMBIO != $NuevoCambio ]]
+then
+
 DIA=`date +"%Y/%m/%d"`
 HORA=`date +"%H:%M"`
-
-
 
 COMPRA=$(curl 127.0.0.1:5000/usd | ../jq .compra)
 VENTA=$(curl 127.0.0.1:5000/usd | ../jq .venta)
 
-
-
-echo "AAAA-MM-AA,HH:MM,Precio Compra,Precio Venta" >> ../historico/historico.txt
-echo "$DIA $HORA,$COMPRA,q$VENTA" >> ../historico/historico.txt
+echo "$DIA $HORA, $COMPRA, $VENTA" >> ../historico/historico.txt
 
 
 
+CAMBIO=$(stat -c %y sync.txt)
 
 
+fi
+
+done
