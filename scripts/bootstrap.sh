b8f727a7e04d3c2c1f76a2b8c15301015b47d621
Alejando <alegodoy2013@gmail.com>
2022-12-16 16:06:51 -0300

    Se actualizo el script bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-ruta="$(pwd)/script.sh"
-echo $ruta
-echo $ruta
-sed -i "s/var/$ruta/g" archivo.txt
+ruta=$(pwd | sed "s/\//-!/g")
 
+echo $ruta
+sed -i "s/\/script.sh/$ruta&/g" servicio.service
+sed -i "s/-!/\//g" servicio.service
+
+cp servicio.service script.sh ../.
 #cp servicio.service tempo.timer /etc/systemd/system
