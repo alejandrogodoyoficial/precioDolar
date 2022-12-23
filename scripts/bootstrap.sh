1bf188e9edc9e7b240fc3d4492668c0d959addae
Alejando <alegodoy2013@gmail.com>
2022-12-23 15:42:16 -0300

    Feat: Se agrego unos archivos y se modifico otros

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,60 @@
 #!/bin/bash
-ruta=$(pwd | sed "s/\//-!/g")
+#ruta=$(pwd | sed "s/\//-!/g")
 
-echo $ruta
-sed -i "s/\/script.sh/$ruta&/g" servicio.service
-sed -i "s/-!/\//g" servicio.service
+#echo $ruta
+#sed -i "s/\/script.sh/$ruta&/g" servicio.service
+#sed -i "s/-!/\//g" servicio.service
 
-cp servicio.service tempo.timer /etc/systemd/system
+#cp servicio.service tempo.timer /etc/systemd/system
+
+# Mostrar el menú al usuario
+echo "Seleccione una opción:"
+echo "1. Instalar"
+echo "2. Desinstalar"
+echo "3. Ejecutar"
+echo "4. Salir"
+
+# Leer la opción del usuario
+read -p "Opción: " option
+
+# Bucle while para procesar la opción del usuario
+while [ "$option" != "4" ]; do
+  # Procesar la opción del usuario
+  case $option in
+    "1")
+	ruta=$(pwd | sed "s/\//-!/g")
+
+	echo $ruta
+	sed -i "s/var/$ruta&/g" servicio.service
+	sed -i "s/-!/\//g" servicio.service
+
+	sudo cp servicio.service /etc/systemd/system
+	sudo cp tempo.timer /etc/systemd/system
+	sudo systemcl daemon-reload
+	sudo systemctl start servicio.timer
+	sudo docker-compose up --build
+;;
+    "2")
+	sudo rm /etc/systemd/system/servicio.service
+	sudo rm /etc/systemd/systemo/tempo.timer
+;;
+    "3")
+	./script.sh
+;;
+    *)
+      # Opción inválida
+      echo "Opción inválida"
+      ;;
+  esac
+
+  # Mostrar el menú de nuevo y leer la opción del usuario
+  echo "Seleccione una opción:"
+  echo "1. Instalar"
+  echo "2. Desinstalar"
+  echo "3. Ejecutar"
+  echo "4. Salir"
+  read -p "Opción: " option
+done
+
+# Salir del script
+exit 0
